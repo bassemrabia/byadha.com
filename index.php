3810be63495562765def29dfbf2c75aa811c38ca
brabia <bassem.rabia@gmail.com>
2016-12-29 23:50:22 +0300

    First Push

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>-- Festival Byadha 127.0.0.1--</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" >
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style type="text/css">
+body {
+  background-color: #b74948;
+  font-family: "Helvetica Neue", "Segoe UI", Segoe, Helvetica, Arial, "Lucida Grande", sans-serif;
+  font-weight: normal;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+}
+</style>
+</head>
+
+<body>
+<div class="container text-center">
+	<img src="logo.png" />
+</body>
+</html>
